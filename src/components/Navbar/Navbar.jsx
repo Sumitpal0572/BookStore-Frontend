c4a771104da8a9370e867dbf754c030511cacca9
Sumitpal0572 <asmyselfsumit@gmail.com>
2025-01-31 00:36:30 +0530

    Navbar created

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex bg-zinc-800 text-white px-8 py-4 items-center justify-between">
+      <div>
+        <h1 className="text-2xl font-semibold">BookHeaven</h1>
+      </div>
+      <div>Links</div>
+    </div>
+  );
 };
 
 export default Navbar;
