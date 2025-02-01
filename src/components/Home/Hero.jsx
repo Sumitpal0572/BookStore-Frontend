4bf518660858ed3b3c0c2e0c6a133a01b698bd83
Sumitpal0572 <asmyselfsumit@gmail.com>
2025-02-01 23:43:58 +0530

    debug hero section

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -3,18 +3,18 @@ import React from "react";
 const Hero = () => {
   return (
     <div className="h-[75vh] flex">
-      <div className="w-3/6 flex flex-col">
-        <h1 className="text-6xl font-semibold text-yellow-100">
+      <div className="w-full lg:w-3/6 flex flex-col items-center lg:items-start justify-center">
+        <h1 className="text-4xl lg:text-6xl font-semibold text-yellow-100 text-center lg:text-left">
           Discover Your Next Great Read
         </h1>
-        <p className="mt-4 text-xl text-zinc-300">
+        <p className="mt-4 text-xl text-zinc-300 text-center lg:text-left">
           Uncover captivating stories, enriching knowledge , and endless
           inspiration in our curated collection of books
         </p>
         <div className="mt-8">
           <button
-            className="text-yellow-100
-          text-2xl
+            className="text-yellow-100 text-xl
+          lg:text-2xl
           font-semibold
           border
           border-yellow-100
