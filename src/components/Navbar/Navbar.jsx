0f3aed49735479842b205a4a9f072f75ffa9224b
Sumitpal0572 <asmyselfsumit@gmail.com>
2025-02-14 22:40:25 +0530

    fix the code

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -26,46 +26,48 @@ const Navbar = () => {
     },
   ];
   return (
-    <div className="flex bg-zinc-800 text-white px-8 py-4 items-center justify-between">
-      <Link to="/" className="flex items-center">
-        <img
-          className="h-10 me-4"
-          src="https://cdn-icons-png.flaticon.com/128/10433/10433049.png"
-          alt="logo"
-        />
-        <h1 className="text-2xl font-semibold">BookHeaven</h1>
-      </Link>
-      <div className="nav-links-bookheaven block md:flex items-center gap-4">
-        <div className="hidden md:flex gap-4">
-          {links.map((items, i) => (
+    <>
+      <nav className="flex bg-zinc-800 text-white px-8 py-4 items-center justify-between">
+        <Link to="/" className="flex items-center">
+          <img
+            className="h-10 me-4"
+            src="https://cdn-icons-png.flaticon.com/128/10433/10433049.png"
+            alt="logo"
+          />
+          <h1 className="text-2xl font-semibold">BookHeaven</h1>
+        </Link>
+        <div className="nav-links-bookheaven block md:flex items-center gap-4">
+          <div className="hidden md:flex gap-4">
+            {links.map((items, i) => (
+              <Link
+                to={items.link}
+                className="hover:text-blue-500 transititon-all duration-300"
+                key={i}
+              >
+                {items.title}
+              </Link>
+            ))}
+          </div>
+          <div className="hidden md:flex gap-4">
             <Link
-              to={items.link}
-              className="hover:text-blue-500 transititon-all duration-300"
-              key={i}
+              to="/LogIn"
+              className="px-4 py-1 border border-blue-500 rounded  hover:bg-white hover:text-zinc-800 transition-all duratiion-300"
             >
-              {items.title}
+              LogIn
             </Link>
-          ))}
+            <Link
+              to="/SignUp"
+              className="px-4 py-1 bg-blue-500 rounded hover:bg-white hover:text-zinc-800 transition-all duratiion-300"
+            >
+              Sign Up
+            </Link>
+          </div>
+          <button className="text-white text-2xl hover:text-zinc-400">
+            <FaGripLines />
+          </button>
         </div>
-        <div className="hidden md:flex gap-4">
-          <Link
-            to="/LogIn"
-            className="px-4 py-1 border border-blue-500 rounded  hover:bg-white hover:text-zinc-800 transition-all duratiion-300"
-          >
-            LogIn
-          </Link>
-          <Link
-            to="/SignUp"
-            className="px-4 py-1 bg-blue-500 rounded hover:bg-white hover:text-zinc-800 transition-all duratiion-300"
-          >
-            Sign Up
-          </Link>
-        </div>
-        <button className="text-white text-2xl">
-          <FaGripLines />
-        </button>
-      </div>
-    </div>
+      </nav>
+    </>
   );
 };
 
