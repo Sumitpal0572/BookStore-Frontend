4a66f8103eabb5c197653c07261bc195e88ab1f0
Sumitpal0572 <asmyselfsumit@gmail.com>
2025-03-01 17:37:12 +0530

    heading are added

--- src/components/Home/RecentlyAdded.jsx
+++ src/components/Home/RecentlyAdded.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const RecentlyAdded = () => {
   return (
-    <div>RecentlyAdded</div>
-  )
-}
+    <div className="mt-8 px-4 ">
+      <h4>Recently Added Books </h4>
+    </div>
+  );
+};
 
-export default RecentlyAdded
+export default RecentlyAdded;
