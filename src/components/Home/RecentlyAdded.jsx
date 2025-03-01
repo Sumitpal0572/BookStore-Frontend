6bbde85d25fa43213ac59d1aafbba6fce5bddc48
Sumitpal0572 <asmyselfsumit@gmail.com>
2025-03-01 17:40:37 +0530

    style added

--- src/components/Home/RecentlyAdded.jsx
+++ src/components/Home/RecentlyAdded.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const RecentlyAdded = () => {
   return (
     <div className="mt-8 px-4 ">
-      <h4>Recently Added Books </h4>
+      <h4 className="text-3xl text-yellow-100">Recently Added Books </h4>
     </div>
   );
 };
