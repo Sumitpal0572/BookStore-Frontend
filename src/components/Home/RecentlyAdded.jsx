fa9a4db260e221d272683b66b04cdd38e442de40
Sumitpal0572 <asmyselfsumit@gmail.com>
2025-03-03 17:01:01 +0530

    fetched books from backend

--- src/components/Home/RecentlyAdded.jsx
+++ src/components/Home/RecentlyAdded.jsx
@@ -1,9 +1,17 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const RecentlyAdded = () => {
-  const [Data, setData] = useState(second);
-  useEffect(() => {}, []);
+  const [Data, setData] = useState();
+  useEffect(() => {
+    const fetch = async () => {
+      const response = await axios.get(
+        "http://localhost:3000/api/v1/get-recent-books"
+      );
+      console.log(response);
+    };
+    fetch();
+  }, []);
   return (
     <div className="mt-8 px-4 ">
       <h4 className="text-3xl text-yellow-100">Recently Added Books </h4>
