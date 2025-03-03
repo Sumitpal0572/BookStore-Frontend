10974dbbac6cd6083675cea7aaceafcfda729632
Sumitpal0572 <asmyselfsumit@gmail.com>
2025-03-03 16:46:02 +0530

    book card setup done

---
+++ src/components/Bookcard/bookcard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const bookcard = () => {
+    
+  return (
+    <div>bookcard</div>
+  )
+}
+
+export default bookcard
--- src/components/Home/RecentlyAdded.jsx
+++ src/components/Home/RecentlyAdded.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 
 const RecentlyAdded = () => {
+  const [Data, setData] = useState(second);
+  useEffect(() => {}, []);
   return (
     <div className="mt-8 px-4 ">
       <h4 className="text-3xl text-yellow-100">Recently Added Books </h4>
