89ede0b8f505af9b6c650cd5ee1eb1bcb8166d78
Sumitpal0572 <asmyselfsumit@gmail.com>
2025-03-16 22:30:21 +0530

    book fetched

--- src/components/Home/RecentlyAdded.jsx
+++ src/components/Home/RecentlyAdded.jsx
@@ -8,13 +8,14 @@ const RecentlyAdded = () => {
       const response = await axios.get(
         "http://localhost:2000/api/v1/get-recent-books"
       );
-      console.log(response);
+      setData(response.data.data);
     };
     fetch();
   }, []);
   return (
     <div className="mt-8 px-4 ">
       <h4 className="text-3xl text-yellow-100">Recently Added Books </h4>
+      <div className="my-4 grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-4"></div>
     </div>
   );
 };
