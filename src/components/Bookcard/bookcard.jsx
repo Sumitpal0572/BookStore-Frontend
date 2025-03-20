b97a56c12b0ab0130de0970d0fdcf1d44249468f
Sumitpal0572 <asmyselfsumit@gmail.com>
2025-03-20 23:48:36 +0530

    debug the code

--- src/components/Bookcard/bookcard.jsx
+++ src/components/Bookcard/bookcard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 const bookcard = ({ data }) => {
+  console.log(data);
   return <div>bookcard</div>;
 };
 
--- src/components/Home/RecentlyAdded.jsx
+++ src/components/Home/RecentlyAdded.jsx
@@ -16,7 +16,7 @@ const RecentlyAdded = () => {
   return (
     <div className="mt-8 px-4 ">
       <h4 className="text-3xl text-yellow-100">Recently Added Books </h4>
-      <div className="my-4 grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-4">
+      <div className="my-8 grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-4">
         {Data &&
           Data.map((items, i) => (
             <div key={i}>
