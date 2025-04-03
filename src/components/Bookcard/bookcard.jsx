580a8d5115d5d24497b5aea169efd6e630aeef3d
Sumitpal0572 <asmyselfsumit@gmail.com>
2025-04-03 14:46:11 +0530

    Loader added

--- src/components/Bookcard/bookcard.jsx
+++ src/components/Bookcard/bookcard.jsx
@@ -5,10 +5,15 @@ const BookCard = ({ data }) => {
   return (
     <>
       <Link>
-        <div className="bg-zinc-800 rounded p-4">
+        <div className="bg-zinc-800 rounded p-4 flex flex-col">
           <div className="bg-zinc-900 rounded flex items-center justify-center">
             <img src={data.url} alt="/" className="h-[25vh]" />
           </div>
+          <h2 className="mt-4 text-xl font-semi-bold">{data.title}</h2>
+          <p className="mt-2 text-zinc-400 font-semibold">by {data.author}</p>
+          <p className="mt-2 text-zinc-200 font-semibold texy-xl">
+            rupees {data.price}
+          </p>
         </div>
       </Link>
     </>
--- src/components/Home/RecentlyAdded.jsx
+++ src/components/Home/RecentlyAdded.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import BookCard from "../Bookcard/BookCard";
+import Loader from "../Loader/Loader";
 
 const RecentlyAdded = () => {
   const [Data, setData] = useState();
@@ -16,6 +17,12 @@ const RecentlyAdded = () => {
   return (
     <div className="mt-8 px-4 ">
       <h4 className="text-3xl text-yellow-100">Recently Added Books </h4>
+      {!Data && (
+        <div className="flex items-center justify-center my-8">
+          {" "}
+          <Loader />
+        </div>
+      )}
       <div className="my-8 grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-4">
         {Data &&
           Data.map((items, i) => (
