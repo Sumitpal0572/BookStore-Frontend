e7b4b5d92f262563e497e2b654ff5946c4c5d5a3
Sumitpal0572 <asmyselfsumit@gmail.com>
2025-04-06 00:02:41 +0530

    Redux added

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import SignUp from "./pages/SignUp";
 import AllBooks from "./pages/AllBooks";
 import LogIn from "./pages/LogIn";
@@ -14,20 +14,18 @@ import ViewBookDetail from "./components/ViewBooksDetails/ViewBookDetail";
 const App = () => {
   return (
     <div>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/about-us" element={<AboutUs />} />
-          <Route path="/all-books" element={<AllBooks />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/Profile" element={<Profile />} />
-          <Route path="/SignUp" element={<SignUp />} />
-          <Route path="/login" element={<LogIn />} />
-          <Route path="view-book-details/:id" element={<ViewBookDetail />} />
-        </Routes>
-        <Footer />
-      </Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/all-books" element={<AllBooks />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/Profile" element={<Profile />} />
+        <Route path="/SignUp" element={<SignUp />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="view-book-details/:id" element={<ViewBookDetail />} />
+      </Routes>
+      <Footer />
     </div>
   );
 };
