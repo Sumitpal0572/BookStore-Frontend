58f60bc8332ffd2e02e5cb703a8782a5355bc652
Sumitpal0572 <asmyselfsumit@gmail.com>
2025-04-05 14:42:20 +0530

    SignUp and Login page crrated

--- src/components/ViewBooksDetails/ViewBookDetail.jsx
+++ src/components/ViewBooksDetails/ViewBookDetail.jsx
@@ -19,11 +19,11 @@ const ViewBookDetail = () => {
   return (
     <>
       {Data && (
-        <div className="px-12 py-8 bg-zinc-900 flex gap-8">
-          <div className="bg-zinc-800 rounded p-4 h-[80vh] w-3/6 flex items-center justify-center">
-            <img src={Data.url} className="h-[70vh]" />
+        <div className=" px-4 md:px-12 py-8 bg-zinc-900 flex flex-col md:flex-row gap-8">
+          <div className="bg-zinc-800 rounded p-4 h-[70vh] lg:h-[80vh] w-full lg:w-3/6 flex items-center justify-center">
+            <img src={Data.url} className="h-[50vh] lg:h-[70vh]" />
           </div>
-          <div className="p-4 w-3/6">
+          <div className="p-4 w-full lg:w-3/6">
             <h1 className="text-4xl text-zinc-300 font-semibold">
               {Data.title}
             </h1>
