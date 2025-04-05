15b3eca8c862c0f098f4871c507c0c1c8503e0b0
Sumitpal0572 <asmyselfsumit@gmail.com>
2025-04-05 12:35:22 +0530

    Description added

--- src/components/ViewBooksDetails/ViewBookDetail.jsx
+++ src/components/ViewBooksDetails/ViewBookDetail.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Loader from "../Loader/Loader";
 import { useParams } from "react-router-dom";
+import { GrLanguage } from "react-icons/gr";
 
 const ViewBookDetail = () => {
   const { id } = useParams();
@@ -16,12 +17,33 @@ const ViewBookDetail = () => {
     fetch();
   }, []);
   return (
-    <div className="px-12 py-8 bg-zinc-900 flex gap-8">
-      <div className="bg-zinc-800 rounded p-4 h-[80vh] w-3/6 flex items-center justify-center">
-        <img src={Data.url} className="h-[70vh]" />
-      </div>
-      <div className="p-4 w-3/6"></div>
-    </div>
+    <>
+      {Data && (
+        <div className="px-12 py-8 bg-zinc-900 flex gap-8">
+          <div className="bg-zinc-800 rounded p-4 h-[80vh] w-3/6 flex items-center justify-center">
+            <img src={Data.url} className="h-[70vh]" />
+          </div>
+          <div className="p-4 w-3/6">
+            <h1 className="text-4xl text-zinc-300 font-semibold">
+              {Data.title}
+            </h1>
+            <p className="text-zinc-400 mt-1">by {Data.author}</p>
+            <p className="text-zinc-500 mt-4 tetx-xl">{Data.desc}</p>
+            <p className="flex mt-4 items-center justify-start text-zinc-400">
+              <GrLanguage className="me-3" /> {Data.language}
+            </p>
+            <p className="mt-4 text-zinc-100 text-xl font-semibold">
+              price : rupees {Data.price}
+            </p>
+          </div>
+        </div>
+      )}
+      {!Data && (
+        <div className="h-screen bg-zinc-900 flex items-center justify-center">
+          <Loader />
+        </div>
+      )}
+    </>
   );
 };
 
