411366b6d8d0cea5c360da86290b01d4ecb7b8dd
Sumitpal0572 <asmyselfsumit@gmail.com>
2025-04-09 00:41:40 +0530

    settings done successfully

--- src/components/Profile/Settings.jsx
+++ src/components/Profile/Settings.jsx
@@ -22,7 +22,39 @@ const Settings = () => {
     };
     fetch();
   }, []);
-  return <div>Settings</div>;
+  return (
+    <>
+      {!ProfileData && <Loader />}
+      {ProfileData && (
+        <div className="h-[100%] p-0 md:p-4 text-zinc-100">
+          <h1 className="text-3xl md:text-5xl font-semibold text-zinc-500 mb-8">
+            Settings
+          </h1>
+          <div className="flex gap-12">
+            <div className="">
+              <label htmlFor="">Email</label>
+              <p className="p-2 rounded bg-zinc-800 mt-2 font-semibold">
+                {ProfileData.email}
+              </p>
+            </div>
+          </div>
+          <div className="mt-4 flex flex-col">
+            <label htmlFor="">Address</label>
+            <textarea
+              className="p-2 rounded bg-zinc-800 mt-2 font-semibold "
+              rows="5"
+              placeholder="Address"
+              name="address"
+              value={Value.address}
+            ></textarea>
+          </div>
+          <div className="mt-4 flex justify-end">
+             <button className="bg-yellow-500 text-zinc-900 font-semibold px-3 py-2 rounded hover:bg-yellow-400">Update</button>
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Settings;
