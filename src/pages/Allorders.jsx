c3710c2147803164293832ad3a37ab63c063a6ad
Sumitpal0572 <asmyselfsumit@gmail.com>
2025-04-10 12:59:25 +0530

    All orders updated

--- src/pages/Allorders.jsx
+++ src/pages/Allorders.jsx
@@ -5,6 +5,7 @@ import Loader from "../components/Loader/Loader";
 import { FaUserLarge } from "react-icons/fa6";
 const Allorders = () => {
   const [AllOrders, setAllOrders] = useState();
+  const [Options, setOptions] = useState(-1);
   const headers = {
     id: localStorage.getItem("id"),
     authorization: `Bearer ${localStorage.getItem("token")}`,
@@ -19,7 +20,10 @@ const Allorders = () => {
     };
     fetch();
   }, []);
-
+  const setOptionsButton = (i) => {
+    setOptions(i);
+  };
+  AllOrders && AllOrders.splice(AllOrders.length - 1, 1);
   return (
     <>
       {!AllOrders && (
@@ -55,7 +59,58 @@ const Allorders = () => {
             </div>
           </div>
           {AllOrders.map((items, i) => (
-            <div>hello</div>
+            <div className="bg-zinc-800 w-full rounded py-2 px-4 flex gap-2 hover:bg-zinc-900 cursor-pointer transition-all duration-300 ">
+              <div className="w-[3%]">
+                <h1 className="text-center">{i + 1}</h1>
+              </div>
+              <div className="w-[40%] md:w-[22%]">
+                <Link
+                  to={`/view-book-details/${items.book._id}`}
+                  className="hover:text-blue-300"
+                >
+                  {items.book.title}
+                </Link>
+              </div>
+              <div className="w-0 md:w-[45%] hidden md:block">
+                <h1 className="">{items.book.desc.slice(0, 50)}...</h1>
+              </div>
+              <div className="w-[17%] md:w-[9%] ">
+                <h1 className="">{items.book.price}</h1>
+              </div>
+              <div className="w-[30%] md:w-[16%] ">
+                <h1 className="font-semibold">
+                  <button
+                    className="hover:scale-105 transition-all duration-300"
+                    onClick={() => setOptionsButton(i)}
+                  >
+                    {items.status === "Order Placed" ? (
+                      <div className="text-yellow-500">{items.status}</div>
+                    ) : items.status === "Cancelled" ? (
+                      <div className="text-red-500">{items.status}</div>
+                    ) : (
+                      <div className="text-green-500">{items.status}</div>
+                    )}
+                  </button>
+                  <div className="flex">
+                    <select name="status" id="" className="bg-gray-800">
+                      {[
+                        "OrderPlaced",
+                        "Out Of Delivery",
+                        "Delivered",
+                        "Cancelled",
+                      ].map((items, i) => (
+                        <option value={items} key={i}>
+                          {items}
+                        </option>
+                      ))}
+                    </select>
+                    <button className="text-green-500 hover:text-pink-600 mx-2">
+                      <FaCheck />
+                    </button>
+                  </div>
+                </h1>
+              </div>
+            </div>
           ))}
         </div>
       )}
